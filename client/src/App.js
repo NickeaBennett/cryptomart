194d521a1476ae20ea9694610165116d3b2012a5
NickeaBennett <nickeabennett@gmail.com>
2022-06-25 07:38:42 -0400

    Footer removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Routes, Route, Link} from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import { HeartOutlined } from "@ant-design/icons";
 // Imports components from index.js
 // import { Navbar } from './components';  
 import { Navbar, Home, Cryptocurrency, Exchanges, CoinDetails, Favorites, News} from './components/'; 
 
 import './App.css';
 
-const App = () => {
-    return (
+const App = () => (
         <div className='app'>
             <div className="navbar">
                 <Navbar />
             </div>
             <div className="main-content">
-                {/* Layout component from antd */}
+                {/* Layout component for routing from antd */}
                 <Layout>
                     <div className='routes'>
                         <Routes>
@@ -27,11 +27,24 @@ const App = () => {
                         </Routes>
                     </div>
                 </Layout>
-            </div>
-            <div className="footer">
+                {/* <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        <Link to="/">
+                        CryptoMart 
+                        </Link> <br />
+                        Made with <HeartOutlined /> by Nickea Bennett
+                    </Typography.Title>
+                     Antd Space similar to div 
+                     <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/cryptocurrencies">Coins</Link>
+                        <Link to="/Exchanges">Exchanges</Link>
+                        <Link to="favorites">Favorites</Link>
+                        <Link to="/news">News</Link>
+                    </Space> 
+                </div> */} 
             </div>
         </div>
-    );
-}
+);
 
 export default App;
